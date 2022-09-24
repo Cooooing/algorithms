f72a90e2f70eb53b797d445b350212bd5e4d1b68
2450123 <2450144961@qq.com>
2022-09-24 23:40:07 +0800

    布隆过滤器

--- src/test/java/BloomFilterTest.java
+++ src/test/java/BloomFilterTest.java
@@ -1,4 +1,5 @@
 import BloomFilter.BloomFilter;
+import com.google.common.hash.Funnels;
 import org.junit.Test;
 
 public class BloomFilterTest {
@@ -12,5 +13,17 @@ public class BloomFilterTest {
         System.out.println(bloomFilter.contains("2"));
         System.out.println(bloomFilter.contains("3"));
         System.out.println(bloomFilter.contains("100001"));
+    }
+
+    @Test
+    public void googleBloomFilter() {
+        com.google.common.hash.BloomFilter<Integer> bloomFilter = com.google.common.hash.BloomFilter.create(Funnels.integerFunnel(), 100000, 0.0001);
+        for (int i = 0; i < 100000; i++) {
+            bloomFilter.put(i);
+        }
+        System.out.println(bloomFilter.mightContain(1));
+        System.out.println(bloomFilter.mightContain(2));
+        System.out.println(bloomFilter.mightContain(3));
+        System.out.println(bloomFilter.mightContain(100001));
     }
 }
