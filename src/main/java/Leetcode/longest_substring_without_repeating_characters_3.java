8d74d7d664446caac8e50150152dfbe796b04968
cooooing <2450144961@qq.com>
2022-10-04 23:51:52 +0800

    leetcode

--- src/main/java/Leetcode/longest_substring_without_repeating_characters_3.java
+++ src/main/java/Leetcode/longest_substring_without_repeating_characters_3.java
@@ -1,20 +1,31 @@
 package Leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Stack;
 
 /**
  * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
  */
 public class longest_substring_without_repeating_characters_3 {
-    public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring_1(""));
-        System.out.println(lengthOfLongestSubstring_1(" "));
-        System.out.println(lengthOfLongestSubstring_1("a"));
-        System.out.println(lengthOfLongestSubstring_1("au"));
-        System.out.println(lengthOfLongestSubstring_1("abcabcbb"));
-        System.out.println(lengthOfLongestSubstring_1("bbbbb"));
-        System.out.println(lengthOfLongestSubstring_1("dvdf"));
+
+    /**
+     * 滑动窗口
+     */
+    public static int lengthOfLongestSubstring_2(String s) {
+        int result = 0;
+        // 记录字符最后出现的位置
+        int[] last = new int[128];
+        Arrays.fill(last, -1);
+        // 窗口的开始位置
+        int start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            // 字符ascii码作为下标
+            int index = s.charAt(i);
+            start = Math.max(start, last[index] + 1);
+            result = Math.max(result, i - start + 1);
+            last[index] = i;
+        }
+        return result;
     }
 
     /**
@@ -24,11 +35,11 @@ public class longest_substring_without_repeating_characters_3 {
         int result = 0;
         char[] str = s.toCharArray();
         int temp = 0;
-        HashMap<Character,Integer> hashMap = new HashMap<>();
+        HashMap<Character, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < str.length; i++) {
-            for (int j = i ; j < str.length; j++) {
+            for (int j = i; j < str.length; j++) {
                 if (!hashMap.containsKey(str[j])) {
-                    hashMap.put(str[j],j);
+                    hashMap.put(str[j], j);
                     temp++;
                 } else {
                     break;
